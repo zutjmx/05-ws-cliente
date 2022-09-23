238d42699ebc526430cef7a65aacbe06cc47251e
Jesus Zuñiga <jesuszt@hotmail.com>
2022-09-23 17:26:00 -0500

    Fin de la sección 15

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { connectToServer } from './socket-client';
+import Swal from 'sweetalert2';
 import './style.css'
 
 //import typescriptLogo from './typescript.svg'
@@ -6,13 +7,39 @@ import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <h1>Cliente de Websockets</h1>
+    <h2>Cliente de Websockets</h2>
+
+    <input placeholder="JSON Web Token" id="jwt-token"/>
+    <button id="btn-conectar">Conectar</button>
+    <br/>
+
     <span id="server-status">OffLine</span>
 
     <ul id="clients-id"></ul>
 
+    <form id="message-form">
+      <input placeholder="mensaje" id="message-input"/>
+    </form>
+
+    <h3>Mensajes</h3>
+    <ul id="messages-ul"></ul>
+
   </div>
 `
-connectToServer();
-
+//connectToServer();
 //setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+const jwtToken = document.querySelector<HTMLInputElement>('#jwt-token')!;
+const btnConectar = document.querySelector<HTMLButtonElement>('#btn-conectar')!;
+
+btnConectar.addEventListener('click', () => {
+  if(jwtToken.value.trim().length <= 0) {
+    Swal.fire(
+      'Cliente WebSocket',
+      'Se requiere un JSON Web Token válido',
+      'info'
+    );
+    return;
+  }
+  connectToServer(jwtToken.value.trim());
+});
--- src/socket-client.ts
+++ src/socket-client.ts
@@ -1,16 +1,29 @@
 import { Manager, Socket } from "socket.io-client";
+import Swal from 'sweetalert2';
 
-export const connectToServer = () => {
+let socket: Socket;
 
-    const manager = new Manager('http://localhost:3000/socket.io/socket.io.js');
-    const socket = manager.socket('/');
+export const connectToServer = (token: string) => {
+
+    const manager = new Manager('http://localhost:3000/socket.io/socket.io.js',{
+        extraHeaders: {
+            hola: 'mundo',
+            authentication: token
+        }
+    });
     
-    addListener(socket);
+    socket?.removeAllListeners();
+    socket = manager.socket('/');
+    
+    addListener();
 }
 
-const addListener = (socket: Socket) => {
+const addListener = () => {
     const serverStatusLabel = document.querySelector('#server-status')!;
     const clientsUl = document.querySelector('#clients-id')!;
+    const messageForm = document.querySelector<HTMLFormElement>('#message-form')!;
+    const messageInput = document.querySelector<HTMLInputElement>('#message-input')!;
+    const messagesUl = document.querySelector<HTMLUListElement>('#messages-ul')!;
     
     socket.on('connect', () => {
         console.log('conectado');
@@ -30,6 +43,35 @@ const addListener = (socket: Socket) => {
             `
         });
         clientsUl.innerHTML = clientsHtml;
+    });
+
+    messageForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        if(messageInput.value.trim().length <= 0) {
+            Swal.fire(
+                'Cliente WebSocket',
+                'Se debe de capturar un mensaje',
+                'info'
+            );
+            return;
+        }
+        socket.emit('message-from-client',{
+            id: 'zutjmx', 
+            message: messageInput.value
+        });
+        messageInput.value = '';
+    });
+
+    socket.on('message-from-server', (payload: {fullName: string, message: string}) => {
+        const nuevoMensaje = `
+            <li>
+                <strong>${payload.fullName}</strong>
+                <span>${payload.message}</span>
+            </li>
+        `;
+        const li = document.createElement('li');
+        li.innerHTML = nuevoMensaje;
+        messagesUl.append(li);
     });
 
 }
